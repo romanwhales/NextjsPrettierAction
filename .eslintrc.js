fc45c63523cb57eb1123184c776e044b04663359
Sunmonu-Adedeji Olawale <walesunmonu@gmail.com>
2022-07-25 09:49:28 +0100

    Installed prettier

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "next",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:testing-library/react",
+    "next/core-web-vitals",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+};
